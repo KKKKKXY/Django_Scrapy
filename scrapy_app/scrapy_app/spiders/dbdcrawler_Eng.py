8360e0a93ebb303d17aa5c1d2d744e303be8d9af
xingyuan kang <myaploy@gmail.com>
2021-01-14 19:58:00 +0800

    test

--- scrapy_app/scrapy_app/spiders/dbdcrawler_Eng.py
+++ scrapy_app/scrapy_app/spiders/dbdcrawler_Eng.py
@@ -11,7 +11,7 @@ from scrapy.spiders import CrawlSpider #, Rule
 import openpyxl
 import random
 
-class DBDSpider(scrapy.Spider):
+class DbdcrawlerSpider(CrawlSpider):
     name = "dbdcrawler_Eng"
     headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
